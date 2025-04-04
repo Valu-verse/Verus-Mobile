45c7e1736ade95fa20dfde24d588781904a25c58
Chris <monkins1010@gmail.com>
2025-04-04 09:51:21 +0100

    IOS UPDATES

--- ios/verusMobile/AppDelegate.m
+++ ios/verusMobile/AppDelegate.m
@@ -15,7 +15,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"verusmobile";
+  self.moduleName = @"valumobile";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
