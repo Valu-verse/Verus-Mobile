1ad02723110dda875cc751b663366a6ac71e912a
Meyse <maxtheyse@pm.me>
2025-09-23 16:17:17 +0200

    updated verusid and personhood cards

--- src/containers/Home/HomeWidgets/AttestationWidget.js
+++ src/containers/Home/HomeWidgets/AttestationWidget.js
@@ -1,74 +1,54 @@
+/*
+  AttestationWidget
+  - Redesign: Valu brand gradient background
+  - Copy only: "Get your Proof of Personhood" (icons removed)
+  - Implemented gradient using react-native-svg to avoid adding new dependencies
+*/
 import React from 'react';
-import { View, Dimensions, Image } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import { Card, Paragraph } from 'react-native-paper';
-import Colors from '../../../globals/colors';
-import { AttesationBadge, Valu } from '../../../images/customIcons';
-import Styles from '../../../styles';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Svg, { Defs, LinearGradient as SvgLinearGradient, Stop, Rect } from 'react-native-svg';
 
 const AttestationWidget = props => {
     const { width } = Dimensions.get('window');
+    const cardWidth = width / 2 - 16;
+    const cardHeight = 110;
 
     return (
         <Card
             style={{
-                height: 110,
-                width: width / 2 - 16,
+                height: cardHeight,
+                width: cardWidth,
                 borderRadius: 10,
-                backgroundColor: 'white'
+                backgroundColor: '#0077A9'
             }}
             mode="elevated"
             elevation={5}>
-            <Card.Content>
-                <View
-                    style={{
-                        display: 'flex',
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        justifyContent: 'flex-end',
-                        marginRight: -10,
-                        marginTop: -12,
-                    }}>
-                    <Paragraph style={{ fontSize: 12, color: Colors.primaryColor }}>
-                        {0}
-                    </Paragraph>
-                    <MaterialCommunityIcons
-                        name={'star'}
-                        color={Colors.primaryColor}
-                        size={16}
-                    />
+            <Card.Content style={{ height: cardHeight, padding: 12 }}>
+                {/* Gradient background (Valu brand blues) */}
+                <View style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0, borderRadius: 10, overflow: 'hidden' }} pointerEvents="none">
+                    <Svg width={cardWidth} height={cardHeight}>
+                        <Defs>
+                            <SvgLinearGradient id="valuGradient" x1="0" y1="0" x2="1" y2="1">
+                                <Stop offset="0" stopColor="#00C8FF" />
+                                <Stop offset="1" stopColor="#0077A9" />
+                            </SvgLinearGradient>
+                        </Defs>
+                        <Rect x={0} y={0} width={cardWidth} height={cardHeight} fill="url(#valuGradient)" rx={10} ry={10} />
+                    </Svg>
                 </View>
                 <View
                     style={{
                         display: 'flex',
+                        flex: 1,
                         flexDirection: 'column',
-                        justifyContent: 'space-evenly',
-                        marginTop: -10
+                        alignItems: 'flex-start',
+                        justifyContent: 'flex-end'
                     }}>
-                    <View
-                        style={{
-                            display: 'flex',
-                            flexDirection: 'row',
-                            justifyContent: 'flex-start',
-                        }}>
-                        <Image source={AttesationBadge} style={{
-                            width: '20%',
-                            height: '100%',
-                            resizeMode: "contain",
-                            marginBottom: 20,
-                            marginLeft: -5,
-                            marginRight: 0
-                        }} />
-                        <Paragraph style={{ fontSize: 16, color: Colors.primaryColor, fontWeight: 'bold' }}>
-                            {'Valu Attestation'}
-                        </Paragraph>
-                    </View>
-                    <Paragraph style={{ fontSize: 12, color: Colors.primaryColor, marginTop:20 }}>
-                        {'Get your Valu Proof of PersonHood'}
+                    <Paragraph style={{ fontSize: 18, color: '#FFFFFF', fontWeight: '700', letterSpacing: -0.2, lineHeight: 22 }}>
+                        {'Get your Proof of Personhood'}
                     </Paragraph>
-
                 </View>
-
             </Card.Content>
         </Card>
     );
